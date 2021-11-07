cd35d2243e6dd7b6b8c2bc2b1081bd286240618f
HodgePodge02 <false4956@gmail.com>
2021-11-06 23:13:58 -0400

    learning data types

--- Lesson-One/app.js
+++ Lesson-One/app.js
@@ -2,5 +2,16 @@
 let myName = "ggg";
 const yourName = "egg";
 
-// const docName = document.getElementById("Name");
+// console.log(yourName, myName)
+
+// Data Types
+// Number
+const age = 18;
+const add = 20;
+const sum = age - add;
+
+console.log(age * add + sum);
+
+
+//const docName = document.getElementById("Name");
 // docName.innerText = myName;
